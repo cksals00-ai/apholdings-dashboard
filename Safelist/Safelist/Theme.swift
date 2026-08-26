8b0b24907aaeaa8fb2ad1432b436016c0572b3f6
cksals00-ai <cksals00@gmail.com>
2026-08-26 11:49:51 +0900

    Delete Safelist directory

--- Safelist/Safelist/Theme.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-enum Theme {
-    static let accent   = Color(red: 0.055, green: 0.486, blue: 0.400)   // #0E7C66
-    static let hit      = Color(red: 0.839, green: 0.271, blue: 0.271)   // #D64545
-    static let unknown  = Color(red: 0.455, green: 0.478, blue: 0.510)   // #747A82
-    static let card     = Color(.secondarySystemBackground)
-}
